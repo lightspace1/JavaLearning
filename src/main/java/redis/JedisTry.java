b9777582b5fea41531a2296e83130f33bc065a70
lightspace <mingyaop@gmail.com>
2020-04-06 23:00:00 -0500

    1.2.1 retrieve the keySet from hash

--- src/main/java/redis/JedisTry.java
+++ src/main/java/redis/JedisTry.java
@@ -3,6 +3,7 @@ package redis;
 import redis.clients.jedis.Jedis;
 
 import java.util.Map;
+import java.util.Set;
 
 public class JedisTry {
     public static void main(String[] args) {
@@ -18,8 +19,15 @@ public class JedisTry {
         String name = jedis.hget("user", "name");
         System.out.println(name);
 
-
         Map<String, String> user = jedis.hgetAll("user");
+        //test the key set
+        Set<String> keySet = user.keySet();
+
+        for  (String key : keySet) {
+            String value = user.get(key);
+            System.out.println(key + ":" + value);
+        }
+
         jedis.close();
     }
 }
