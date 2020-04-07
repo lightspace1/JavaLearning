5f27fe8abf9c924745ae455d917b4bde9733547b
lightspace <mingyaop@gmail.com>
2020-04-06 23:25:05 -0500

    1.3 test the JedisPool

---
+++ src/main/java/redis/LearnJedisPoll.java
@@ -0,0 +1,23 @@
+package redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class LearnJedisPoll {
+    public static void main(String[] args) {
+        //setup config
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(50);
+        config.setMaxIdle(10);
+        //create pool object
+        JedisPool jedisPool = new JedisPool(config, "localhost", 6379);
+
+        // get the connection
+        Jedis jedis = jedisPool.getResource();
+
+        // use the connection
+        jedis.set("pooltest", "confirm");
+        jedis.close();
+    }
+}
