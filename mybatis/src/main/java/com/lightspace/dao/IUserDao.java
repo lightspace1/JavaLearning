d18da380decfc05810051dde7ff4ad98a70ffd06
lightspace <mingyaop@gmail.com>
2020-04-14 23:27:37 -0500

    3.1 change mybatis configuration way to annotation

--- mybatis/src/main/java/com/lightspace/dao/IUserDao.java
+++ mybatis/src/main/java/com/lightspace/dao/IUserDao.java
@@ -1,9 +1,11 @@
 package com.lightspace.dao;
 
 import com.lightspace.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IUserDao {
+    @Select("select * from user")
     List<User> findAll();
 }
